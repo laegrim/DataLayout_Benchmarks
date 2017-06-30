315724ecc5d1f8973b58fd89fee9cabefadd79c1
Samuel Teich <steich@gmail.com>
2017-06-29 22:58:43 -0500

    fixed some run_benchmarks bugs

--- run_benchmarks.py
+++ run_benchmarks.py
@@ -9,16 +9,17 @@ import time
 benchmarks_base = {}
 benchmarks_base['benchmarks_base_dir'] = '/home/st1289/rodinia_3.1/cuda/'
 benchmarks_base['data_base_dir'] = '/home/st1289/rodinia_3.1/data/'
-benchmarks_base['args_base'] =  ['--metrics',
-       'gld_transactions_per_request,gst_transactions_per_request',
+benchmarks_base['args_base'] =  [
+       #'--metrics',
+       #'gld_transactions_per_request,gst_transactions_per_request',
        '--log-file',
        'results.txt',
         '--timeout',
         '240',
-		'-u',
-		'ms',
-		'--system-profiling',
-		'on']
+        '-u',
+        'ms',
+        '--system-profiling',
+        'on']
 benchmarks = ['backprop',
               'bfs',
               'b+tree',
@@ -63,7 +64,7 @@ def get_default_run(benchmarks, benchmarks_base):
 
 def get_mem_divergence(benchmark_dir):
         
-	cd(benchmark_dir)
+    cd(benchmark_dir)
 
     time.sleep(2)
     mem_lines = grep('transactions_per_request', 'results.txt')
@@ -82,33 +83,33 @@ def get_mem_divergence(benchmark_dir):
 
 def get_power_stats(benchmark_dir):
 
-	cd(benchmark_dir)
+    cd(benchmark_dir)
 
-	time.sleep(2)
-	time_line = [line for line in grep("Time(%)", "-m", "1", "-A", "2", "test_results.txt").stdout.decode('utf-8').split('\n')]
-	time_line = [stat for stat in line[2].split(' ') if stat != '']
-	power_line = [stat for stat in grep("Power", "test_results.txt").stdout.decode('utf-8').split('\n')[0].split(' ') if stat != '']
-	device = grep("Device", "-m", "1", "test_results.txt").stdout.decode('utf-8').split('\n')[0]
+    time.sleep(2)
+    time_line = [line for line in grep("Time(%)", "-m", "1", "-A", "2", "results.txt").stdout.decode('utf-8').split('\n')]
+    time_line = [stat for stat in line[2].split(' ') if stat != '']
+    power_line = [stat for stat in grep("Power", "results.txt").stdout.decode('utf-8').split('\n')[0].split(' ') if stat != '']
+    device = grep("Device", "-m", "1", "results.txt").stdout.decode('utf-8').split('\n')[0]
 
-	results = {
-		'total_time':float(line[1])/(float(line[0])/100.0),
-		'top_kernel_time_pecent':float(line[0]),
-		'top_kernel_time_ms':float(line[1]),
-		'top_kernel_calls':int(line[2]),
-		'top_kernel_avg':float(line[3]),
-		'top_kernel_min':float(line[4]),
-		'top_kernel_max':float(line[5]),
-		'top_kernel_sig':' '.join(line[6:-1]),
-		'device':device,
-		'power_profiling_count':int(power_line[2]),
-		'avg_mW':float(power_line[3]),
-		'min_mW':float(power_line[4]),
-		'max_mW':float(power_line[5])
-	}
+    results = {
+        'total_time':float(line[1])/(float(line[0])/100.0),
+        'top_kernel_time_pecent':float(line[0]),
+        'top_kernel_time_ms':float(line[1]),
+        'top_kernel_calls':int(line[2]),
+        'top_kernel_avg':float(line[3]),
+        'top_kernel_min':float(line[4]),
+        'top_kernel_max':float(line[5]),
+        'top_kernel_sig':' '.join(line[6:-1]),
+        'device':device,
+        'power_profiling_count':int(power_line[2]),
+        'avg_mW':float(power_line[3]),
+        'min_mW':float(power_line[4]),
+        'max_mW':float(power_line[5])
+    }
 
-	return results
+    return results
 
-def run_benchmark(benchmakr_dir, args, timeout):
+def run_benchmark(benchmark_dir, args, timeout):
 
     cd(benchmark_dir)
     try:
@@ -124,9 +125,9 @@ def run_benchmarks(benchmarks, benchmarks_base, timeout):
         for run in default_runs[benchmark_dir]:
             print(benchmarks_base['args_base'] + run)
             try:
-				run_benchmark(benchmark_dir, benchmarks_base['args_base'] + [run], timeout)
+                run_benchmark(benchmark_dir, benchmarks_base['args_base'] + [run], timeout)
                 #results = get_mem_divergence(benchmark_dir)
-				results = get_power_stats(benchmark_dir)
+                results = get_power_stats(benchmark_dir)
                 results[benchmark_dir].append((run, results))
             except ErrorReturnCode as err:
                 print(err)
@@ -141,22 +142,22 @@ for benchmark in results:
     for run in results[benchmark]:
         if run:
             #row = [benchmark, run[0], run[1]['gld_min'], run[1]['gld_avg'], run[1]['gld_max'], run[1]['gst_min'], run[1]['gst_avg'], run[1]['gst_max']]
-			row = [benchmark, 
-					run[0], 
-					run[1]['total_time'], 
-					run[1]['top_kernel_time_percent'],
-					run[1]['top_kernel_time_ms'],
-					run[1]['top_kernel_calls'],
-					run[1]['top_kernel_avg'],
-					run[1]['top_kernel_min'],
-					run[1]['top_kernel_max'],
-					run[1]['top_kernel_sig'],
-					run[1]['device'],
-					run[1]['power_profiling_count'],
-					run[1]['avg_mW'],
-					run[1]['min_mW'],
-					run[1]['max_mW']]
-				
+            row = [benchmark, 
+                    run[0], 
+                    run[1]['total_time'], 
+                    run[1]['top_kernel_time_percent'],
+                    run[1]['top_kernel_time_ms'],
+                    run[1]['top_kernel_calls'],
+                    run[1]['top_kernel_avg'],
+                    run[1]['top_kernel_min'],
+                    run[1]['top_kernel_max'],
+                    run[1]['top_kernel_sig'],
+                    run[1]['device'],
+                    run[1]['power_profiling_count'],
+                    run[1]['avg_mW'],
+                    run[1]['min_mW'],
+                    run[1]['max_mW']]
+                
             table.append(row)
             
 cd(benchmarks_base['benchmarks_base_dir'])
